09d0f2275f0d78d4cffa7e8ae428eab12de9801e
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2020-05-06 21:25:27 -0700

    Upgraded Unreal Engine to 4.25

---
+++ README.md
@@ -0,0 +1,3 @@
+# CrazyTankies
+
+Developed with Unreal Engine 4
---
+++ Source/CrazyTankies/CrazyTankiesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CrazyTankiesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAZYTANKIES_API ACrazyTankiesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
